5e6f94fed1bbe9767c51f428a68d37323feae719
SNAKE\s-na <593651731@qq.com>
2017-02-15 22:57:06 +0800

    reids的使用

---
+++ src/main/java/com/redis/base/AbstractBaseRedisDao.java
@@ -0,0 +1,32 @@
+package com.redis.base;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public abstract class AbstractBaseRedisDao<K extends Serializable, V extends Serializable>  {
+    
+    @Autowired
+    protected RedisTemplate<K,V> redisTemplate ;
+
+    /** 
+     * 设置redisTemplate 
+     * @param redisTemplate the redisTemplate to set 
+     */   
+    public void setRedisTemplate(RedisTemplate<K, V> redisTemplate) {  
+        this.redisTemplate = redisTemplate;  
+    }  
+      
+    /** 
+     * 获取 RedisSerializer 
+     * <br>------------------------------<br> 
+     */  
+    protected RedisSerializer<String> getRedisSerializer() {  
+        return redisTemplate.getStringSerializer();  
+    }  
+    
+}
